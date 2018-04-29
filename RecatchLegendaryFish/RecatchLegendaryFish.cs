f1a896425055fbed5c97aa211ea514a41d73fe01
Jesse Plamondon-Willard <github@jplamondonw.com>
2018-04-29 14:40:03 -0400

    rewrite RecatchLegendaryFish
    The mod now only stashes while the player is fishing (which avoids issues like legendaries not appearing in the collections tab), and merges fish data when restoring (so players get fishing stats for legendaries too).

--- RecatchLegendaryFish/RecatchLegendaryFish.cs
+++ RecatchLegendaryFish/RecatchLegendaryFish.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using RecatchLegendaryFish.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
+using StardewValley.Tools;
 
 namespace RecatchLegendaryFish
 {
@@ -13,17 +13,8 @@ namespace RecatchLegendaryFish
         /*********
         ** Properties
         *********/
-        /// <summary>Get whether the mod is loaded and ready to intercept catches.</summary>
-        private bool IsLoaded;
-
-        /// <summary>The number of unique fish previously caught by the player.</summary>
-        private int FishTypesCaught;
-
-        /// <summary>The fish IDs for legendary fish.</summary>
-        private readonly int[] LegendaryFishIDs = { 159, 160, 163, 682, 775 };
-
-        /// <summary>A backup of legendaries caught by the current player.</summary>
-        private readonly IDictionary<int, int[]> Stash = new Dictionary<int, int[]>();
+        /// <summary>Temporarily hides caught legendary fish from the game.</summary>
+        private readonly FishStash Stash = new FishStash();
 
 
         /*********
@@ -35,13 +26,10 @@ namespace RecatchLegendaryFish
         {
             SaveEvents.AfterLoad += this.SaveEvents_AfterLoad;
             SaveEvents.BeforeSave += this.SaveEvents_BeforeSave;
-            SaveEvents.AfterSave += this.SaveEvents_AfterSave;
-            SaveEvents.AfterReturnToTitle += this.SaveEvents_AfterReturnToTitle;
-
             GameEvents.UpdateTick += this.ReceiveUpdateTick;
         }
 
-        
+
         /*********
         ** Private methods
         *********/
@@ -53,11 +41,7 @@ namespace RecatchLegendaryFish
         /// <param name="e">The event arguments.</param>
         private void SaveEvents_AfterLoad(object sender, EventArgs e)
         {
-            // reset legendary catches when a game starts
             this.Stash.Clear();
-            this.StashLegendaries();
-            this.FishTypesCaught = Game1.player.fishCaught.Count;
-            this.IsLoaded = true;
         }
 
         /// <summary>The method called before the player saves the game.</summary>
@@ -65,25 +49,7 @@ namespace RecatchLegendaryFish
         /// <param name="e">The event arguments.</param>
         private void SaveEvents_BeforeSave(object sender, EventArgs e)
         {
-            // restore legendaries before save
-            this.RestoreLegendaries();
-        }
-
-        /// <summary>The method called after the player saves the game.</summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event arguments.</param>
-        private void SaveEvents_AfterSave(object sender, EventArgs e)
-        {
-            // remove legendaries after save
-            this.StashLegendaries();
-        }
-
-        /// <summary>The method called after the player exits to the title screen.</summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event arguments.</param>
-        private void SaveEvents_AfterReturnToTitle(object sender, EventArgs e)
-        {
-            this.IsLoaded = false;
+            this.Stash.Restore(); // just in case something weird happens
         }
 
         /// <summary>The method called when the game updates state.</summary>
@@ -91,55 +57,18 @@ namespace RecatchLegendaryFish
         /// <param name="e">The event arguments.</param>
         void ReceiveUpdateTick(object sender, EventArgs e)
         {
-            if (!Game1.hasLoadedGame || !this.IsLoaded)
+            if (!Context.IsWorldReady)
                 return;
 
-            // intercept any caught legendaries
-            if (Game1.player.fishCaught.Count != this.FishTypesCaught)
+            // stash legendaries while fishing
+            bool isFishing = Game1.player.UsingTool && Game1.player.CurrentTool is FishingRod;
+            if (isFishing)
             {
-                // trigger achievement check
-                this.RestoreLegendaries();
-                Game1.stats.checkForFishingAchievements();
-
-                // mark legendaries uncaught
-                this.StashLegendaries();
-
-                // update number caught
-                this.FishTypesCaught = Game1.player.fishCaught.Count;
+                if (!this.Stash.IsStashed)
+                    this.Stash.Start();
             }
-        }
-
-        /****
-        ** Methods
-        ****/
-        /// <summary>Mark all legendary fish uncaught, stashing the previous catch data for safekeeping.</summary>
-        private void StashLegendaries()
-        {
-            foreach (int fishID in this.LegendaryFishIDs)
-            {
-                if (Game1.player.fishCaught.TryGetValue(fishID, out int[] freshValues))
-                {
-                    // remember new catches
-                    this.Stash[fishID] = this.Stash.TryGetValue(fishID, out int[] stashedValues)
-                        ? stashedValues.Union(freshValues).ToArray()
-                        : freshValues;
-
-                    // mark uncaught
-                    Game1.player.fishCaught.Remove(fishID);
-                }
-            }
-        }
-
-        /// <summary>Restore any previous legendary catches from the stash.</summary>
-        private void RestoreLegendaries()
-        {
-            foreach (int fishID in this.Stash.Keys)
-            {
-                Game1.player.fishCaught[fishID] = Game1.player.fishCaught.TryGetValue(fishID, out int[] currentValues)
-                    ? this.Stash[fishID].Union(currentValues).ToArray()
-                    : this.Stash[fishID];
-            }
-            this.Stash.Clear();
+            else if (this.Stash.IsStashed)
+                this.Stash.Restore();
         }
     }
 }
