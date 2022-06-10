73a465e7b4c73ac2afc171b4347f0e9562fcb94d
Tilwa Qendov <tilwa.qendov@gmail.com>
2022-06-10 20:53:02 +0200

    Move files around for refactor git history

--- AllProfessions/Framework/ModData.cs
+++
@@ -1,9 +0,0 @@
-namespace AllProfessions.Framework
-{
-    /// <summary>The mod configuration model.</summary>
-    internal class ModData
-    {
-        /// <summary>The professions to gain for each level.</summary>
-        public ModDataProfessions[] ProfessionsToGain { get; set; }
-    }
-}
--- _archived/AllCropsAllSeasons/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("All Crops All Seasons")]
-[assembly: AssemblyVersion("1.6.1")]
-[assembly: AssemblyFileVersion("1.6.1")]
